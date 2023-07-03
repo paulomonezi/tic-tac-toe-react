71e7e32bb32ef87cbb2a732aaa2cecde74056c58
Paulo Monezi <paulo.monezi@icloud.com>
2023-07-03 19:28:31 -0400

    feat(app.jsx): added game rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,107 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.scss"
+import ReloadBtn from "./components/buttons/reloadButton"
 
-/*
-  DESAFIO TÉCNICO - JOGO DA VELHA - por fernandev
+const winningCombinations = [
+  // horizontals
+  { indexes: [0, 1, 2], orientation: "horizontal" },
+  { indexes: [3, 4, 5], orientation: "horizontal" },
+  { indexes: [6, 7, 8], orientation: "horizontal" },
 
-  * descrição
-    desenvolva um jogo da velha (tic tac toe) funcional.
-    use qualquer técnica de estilização preferida: css modules, sass, styled.
+  //verticals
+  { indexes: [0, 3, 6], orientation: "vertical" },
+  { indexes: [1, 4, 7], orientation: "vertical" },
+  { indexes: [2, 5, 8], orientation: "vertical" },
 
-  * tasks
-    ? - crie um board de 3x3
-    ? - dois jogadores
-    ? - ao clicar em um quadrado, preencher com a jogada
-    ? - avisar quando o jogo finalizar, caso dê velha avise também
-    ? - fazer um risco na sequência vencedora, caso houver
-*/
+  //diagonals
+  { indexes: [0, 4, 8], orientation: "diagonal-1" },
+  { indexes: [2, 4, 6], orientation: "diagonal-2" },
+]
 
 function App() {
   const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
+  const [turn, setTurn] = useState(1)
+  const [winningPlayer, setWinningPlayer] = useState(null)
 
-  const handleClick = () => {
-    console.log('Clicked')
+  const handleClick = (clickedIndex) => {
+    if (gameData[clickedIndex] !== 0) {
+      return
+    }
+    if (winningPlayer) {
+      return
+    }
+    setGameData((prev) => {
+      const newGameData = [...prev]
+      newGameData[clickedIndex] = turn
+      return newGameData
+    })
+
+    setTurn((prev) => (prev === 1 ? 2 : 1))
+  }
+
+  useEffect(() => {
+    checkWinner()
+    checkGameEnded()
+  }, [gameData])
+  let winner = null
+
+  const checkGameEnded = () => {
+    if (gameData.every((item) => item !== 0 && winner == null)) {
+      setTimeout(() => {
+        alert("Deu velha!")
+      }, 20)
+    }
+  }
+  const checkWinner = () => {
+    for (let combination of winningCombinations) {
+      const { indexes } = combination
+      if (
+        gameData[indexes[0]] === 1 &&
+        gameData[indexes[1]] === 1 &&
+        gameData[indexes[2]] === 1
+      ) {
+        winner = "Jogador 1"
+      }
+      if (
+        gameData[indexes[0]] === 2 &&
+        gameData[indexes[1]] === 2 &&
+        gameData[indexes[2]] === 2
+      ) {
+        winner = "Jogador 2"
+      }
+      if (winner) {
+        setWinningPlayer(combination)
+        setTimeout(() => {
+          alert(`${winner} venceu`)
+        }, 20)
+
+        break
+      }
+    }
   }
   return (
     <>
-      <div className="board-game">
-        {gameData.map((value) =>(
-          <span onClick={handleClick}>{value}</span>
-        ))}
+      <div className="game-area">
+        <div className="board-game">
+          {gameData.map((value, index) => (
+            <span
+              key={index}
+              className={
+                winningPlayer?.indexes.includes(index)
+                  ? winningPlayer.orientation
+                  : undefined
+              }
+              onClick={() => {
+                handleClick(index)
+              }}
+            >
+              <abbr title="">{index}</abbr>
+              {value === 1 && "❌"}
+              {value === 2 && "⭕"}
+            </span>
+          ))}
+        </div>
+        <ReloadBtn/>
       </div>
     </>
   )
